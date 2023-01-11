2c644e2f8bca784a0009b80035168a24c3e1f65c
GuangChen2333 <guangchenworks@outlook.com>
2023-01-11 21:40:14 +0800

    feat(plugin-test): add info test

--- plugin-test/src/main/java/cn/guangchen233/crystallistener/test/Main.java
+++ plugin-test/src/main/java/cn/guangchen233/crystallistener/test/Main.java
@@ -14,6 +14,21 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerDeathWithCrystal(PlayerDeathByPlayerWithCrystalEvent event) {
         getLogger().info("Successful listen the event");
+        getLogger().info("Original Damage: " + event.getDamage());
+        getLogger().info("Final Damage: " + event.getFinalDamage());
+        getLogger().info("Killer: " + event.getKiller());
+        getLogger().info("Deceased: " + event.getDeceased());
+        getLogger().info("Crystal: " + event.getCrystal());
+        getLogger().info("Keep Inventory: " + event.getKeepInventory());
+        getLogger().info("Keep Level: " + event.getKeepLevel());
+        getLogger().info("New Exp: " + event.getNewExp());
+        getLogger().info("New Level: " + event.getNewLevel());
+        getLogger().info("New Total Exp: " + event.getNewTotalExp());
+        getLogger().info("Dropped Exp:" + event.getDroppedExp());
+        getLogger().info("Drops: " + event.getDrops());
+        getLogger().info("Suicide: " + event.isSuicide());
+        getLogger().info("Old death message: " + event.getDeathMessage());
         event.setDeathMessage("Success to set death message");
+        getLogger().info("New death message: " + event.getDeathMessage());
     }
 }
